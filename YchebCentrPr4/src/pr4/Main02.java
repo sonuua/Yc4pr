e3c315b669881dd2aa76f812a7870ffaeadd4261
Admin <sonatronkova@gmail.com>
2025-11-27 21:40:18 +0300

    our commit 2

---
+++ YchebCentrPr4/src/pr4/Main02.java
@@ -0,0 +1,11 @@
+package pr4;
+
+public class Main02 {
+
+    public static void main(String[] args) {
+        System.out.println("Это второй тестовый класс.");
+        System.out.println("Он добавлен после первого коммита.");
+        System.out.println("Проверка отправки изменений в GitHub.");
+    }
+}
+
