84f9ae7bb8a7853cf5d9b02d7a86399362220245
Admin <sonatronkova@gmail.com>
2025-11-27 21:34:23 +0300

    our commit

--- YchebCentrPr4/src/pr4/Main1.java
+++ YchebCentrPr4/src/pr4/Main1.java
@@ -2,4 +2,8 @@ package pr4;
 
 public class Main1 {
 
+    public static void main(String[] args) {
+        System.out.println("Это первый тестовый класс для GitHub.");
+        System.out.println("Проверка успешной интеграции Eclipse и GitHub.");
+    }
 }
